3e7ba2636d38befa55299fc15251b42d8a8a5d9b
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-09-30 14:47:05 +0700

    Comment to make Actions pass

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 import authorRouter from "./routes/authorRouter.js";
 // const bookRouter = require("./routes/bookRouter");
 // const indexRouter = require("./routes/indexRouter");
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient();
 
 app.use("/authors", authorRouter);
 // app.use("/books", bookRouter);
@@ -22,19 +22,21 @@ app.listen(PORT, (error) => {
   console.log(`Listening to http://localhost:${PORT}`);
 });
 
+/*
+
 app.get('/', async (req, res) => {
-  // await prisma.user.create({
-  //   data: {
-  //     name: 'Alice',
-  //     email: 'alice@prisma.io',
-  //     posts: {
-  //       create: { title: 'Hello World', content: 'hey can i set this post content?' },
-  //     },
-  //     profile: {
-  //       create: { bio: 'I like turtles' },
-  //     },
-  //   },
-  // })
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World', content: 'hey can i set this post content?' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
 
   const allUsers = await prisma.user.findMany({
     include: {
@@ -52,4 +54,5 @@ app.get('/', async (req, res) => {
   res.json(allUsers);
 });
 
+*/
 
