7366fa71f25e36b6bb2c9f3d4f6f0d73d34c4f3c
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-09-30 10:38:35 +0700

    Experiment with Prisma successfully

--- server/src/index.js
+++ server/src/index.js
@@ -3,36 +3,53 @@ import dotenv from "dotenv";
 import authorRouter from "./routes/authorRouter.js";
 // const bookRouter = require("./routes/bookRouter");
 // const indexRouter = require("./routes/indexRouter");
-import { Pool } from "pg";
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const prisma = new PrismaClient()
 
 app.use("/authors", authorRouter);
 // app.use("/books", bookRouter);
 // app.use("/", indexRouter);
 
-app.get('/', async (req, res) => {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-
-  const client = await pool.connect();
-  const result = await client.query('SELECT version()');
-
-  client.release();
-
-  const { version } = result.rows[0];
-  
-  res.json({ version });
-});
-
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-
   console.log(`Listening to http://localhost:${PORT}`);
 });
+
+app.get('/', async (req, res) => {
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Hello World', content: 'hey can i set this post content?' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+
+  await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true },
+  })
+
+
+  res.json(allUsers);
+});
+
+
