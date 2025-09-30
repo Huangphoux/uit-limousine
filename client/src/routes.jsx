d1e38590e7168eec86233d7cacafb4cbb6d3d9be
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-09-30 14:43:45 +0700

    Add React Router

---
+++ client/src/routes.jsx
@@ -0,0 +1,17 @@
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import ErrorPage from "./pages/ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+];
+
+export default routes;
