00c13f5b10fc46c38589a5c1db337f503604c87b
Greate-Seagull <h0274.hmt@gmail.com>
2025-10-05 11:44:59 +0700

    Remove bad sign-up test

--- server/src/presentation_layer/routes/signUpRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import createUser from '../controllers/signUpController.js';
-
-const signUpRouter = Router();
-
-signUpRouter.post("/api/sign-up", createUser);
-
-export default signUpRouter;
