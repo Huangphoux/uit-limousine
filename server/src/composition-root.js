545b32399880a825067adc0090d957adcc4e6ae1
Greate-Seagull <h0274.hmt@gmail.com>
2025-10-18 17:11:15 +0700

    Set up dependency injection root

--- server/src/composition-root.js
+++ server/src/composition-root.js
@@ -1,9 +1,13 @@
-import { LoginController } from "./presentation_layer/controllers/login.controller";
-import { LoginUseCase } from "./application_layer/login.usecase";
-import { UserRepositoryPostgree } from "./infrustructure_layer/user.repository.postgree"
-import { TokenRepositoryPostgree } from "./infrustructure_layer/token.repository.postgree"
+import { config } from "./config.js"
+
+import { LoginController } from "./presentation_layer/controllers/login.controller.js";
+import { LoginUseCase } from "./application_layer/login.usecase.js";
+import { UserRepositoryPostgree } from "./infrustructure_layer/user.repository.postgree.js"
+import { TokenRepositoryPostgree } from "./infrustructure_layer/token.repository.postgree.js"
 
 const userRepository = new UserRepositoryPostgree()
 const tokenRepository = new TokenRepositoryPostgree()
-const loginUseCase = new LoginUseCase(userRepository, tokenRepository)
+const loginUseCase = new LoginUseCase(userRepository, tokenRepository, config.jwt.secret, config.jwt.expiry)
 export const loginController = new LoginController(loginUseCase)
+
+console.log(config.jwt.secret)
