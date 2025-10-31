39fe8e0c6a1d9e3c9bd62db0f79a2c740966d0e0
TUNGBUI6705 <buitung6705@gmail.com>
2025-10-31 13:21:26 +0700

    update

--- server/src/presentation_layer/routes/course.routes.js
+++ server/src/presentation_layer/routes/course.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+<<<<<<< HEAD
 import { getCourseById, getAllCourses } from '../controllers/courses/course.controller.js';
+=======
+import { getCourseById } from '../controllers/course.controller.js';
+>>>>>>> parent of 3709e3a (update code)
 
 const router = Router();
 
-router.get('/', getAllCourses);        
-router.get('/:id', getCourseById);     
+// Route lấy chi tiết khoá học
+router.get('/:id', getCourseById);
 
 export default router;
