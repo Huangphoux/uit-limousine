c149395511dc876e7028c281db568b13693c54a5
TUNGBUI6705 <buitung6705@gmail.com>
2025-11-17 22:45:43 +0700

    update api

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import authRouter from './presentation_layer/routes/auth.route.js';
 import coursesRouter from './presentation_layer/routes/courses.route.js';
 import lessonRouter from './presentation_layer/routes/lessons.router.js';
+import notificationRouter from './presentation_layer/routes/notification.route.js';
+import gradeRouter from './presentation_layer/routes/grade.route.js'; 
+
 
 const app = express();
 app.use(cors({ origin: ['https://uit-limousine.netlify.app', 'http://localhost:5173'] }));
@@ -12,5 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/auth', authRouter);
 app.use('/courses', coursesRouter);
 app.use('/lessons', lessonRouter);
+app.use('/notifications', notificationRouter);
+app.use('/grade', gradeRouter);
 
 export default app;
