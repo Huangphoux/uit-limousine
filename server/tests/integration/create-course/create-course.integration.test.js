2acbc7a2f5d3f8816f8d43d32d3b3e3bd8f1f07d
Huy Vu <23520657@gm.uit.edu.vn>
2025-12-03 15:50:22 +0700

    Fix create-course integration test - ensure local test passes

--- server/tests/integration/create-course/create-course.integration.test.js
+++ server/tests/integration/create-course/create-course.integration.test.js
@@ -1,75 +1,74 @@
 import { outputSchema } from "../../../src/application_layer/courses/create-course.usecase";
-import { prisma, createCourseUsecase } from "../../../src/composition-root"
+import { prisma, createCourseUsecase } from "../../../src/composition-root";
 import { input, instructor } from "./test-data";
 
 jest.setTimeout(20000);
 
-describe('Create course integration test', () => {
+describe("Create course integration test", () => {
+  beforeAll(async () => {
+    await prisma.user.create({ data: instructor });
+  });
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: instructor.id } });
+  });
+
+  describe("Normal case", () => {
+    let test_input = input;
+    let test_output;
+
     beforeAll(async () => {
-        await prisma.user.create({ data: instructor });
-    })
-
-    afterAll(async () => {
-        await prisma.user.delete({ where: { id: instructor.id } });
-    })
-
-    describe('Normal case', () => {
-        let test_input = input;
-        let test_output;
-
-        beforeAll(async () => {
-            try {
-                test_output = await createCourseUsecase.execute(test_input);
-            }
-            catch (e) {
-                test_output = e;
-            }
-        });
-
-        it(`Should return object match the schema`, () => {
-            expect(() => outputSchema.parse(test_output)).not.toThrow();
-        });
+      try {
+        test_output = await createCourseUsecase.execute(test_input);
+      } catch (e) {
+        test_output = e;
+      }
     });
 
-    // describe('Abnormal case', () => {
-    //     describe('Not found user case', () => {
-    //         let test_input = structuredClone(input);
-    //         let test_output;
+    it(`Should return object match the schema`, () => {
+      expect(() => outputSchema.parse(test_output)).not.toThrow();
+    });
+  });
 
-    //         beforeAll(async () => {
-    //             test_input.id = "unknown";
+  // describe('Abnormal case', () => {
+  //     describe('Not found user case', () => {
+  //         let test_input = structuredClone(input);
+  //         let test_output;
 
-    //             try {
-    //                 test_output = await changeRoleUsecase.execute(test_input);
-    //             }
-    //             catch (e) {
-    //                 test_output = e;
-    //             }
-    //         });
+  //         beforeAll(async () => {
+  //             test_input.id = "unknown";
 
-    //         it(`Should return error message`, () => {
-    //             expect(test_output.message).toBe(`User not found, ${test_input.id}`);
-    //         });
-    //     });
+  //             try {
+  //                 test_output = await changeRoleUsecase.execute(test_input);
+  //             }
+  //             catch (e) {
+  //                 test_output = e;
+  //             }
+  //         });
 
-    //     describe('Not found role case', () => {
-    //         let test_input = structuredClone(input);
-    //         let test_output;
+  //         it(`Should return error message`, () => {
+  //             expect(test_output.message).toBe(`User not found, ${test_input.id}`);
+  //         });
+  //     });
 
-    //         beforeAll(async () => {
-    //             test_input.role = "unknown";
+  //     describe('Not found role case', () => {
+  //         let test_input = structuredClone(input);
+  //         let test_output;
 
-    //             try {
-    //                 test_output = await changeRoleUsecase.execute(test_input);
-    //             }
-    //             catch (e) {
-    //                 test_output = e;
-    //             }
-    //         });
+  //         beforeAll(async () => {
+  //             test_input.role = "unknown";
 
-    //         it(`Should return error message`, () => {
-    //             expect(test_output.message).toBe(`Role not found, ${test_input.role}`);
-    //         });
-    //     });
-    // });
+  //             try {
+  //                 test_output = await changeRoleUsecase.execute(test_input);
+  //             }
+  //             catch (e) {
+  //                 test_output = e;
+  //             }
+  //         });
+
+  //         it(`Should return error message`, () => {
+  //             expect(test_output.message).toBe(`Role not found, ${test_input.role}`);
+  //         });
+  //     });
+  // });
 });
